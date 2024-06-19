6b87cb28ba5d1256f4e63a5a8ecb3d3e20b54b91
alvarado0925 <alvarado0925@gmail.com>
2024-06-18 22:43:34 -0500

    Implementa una composición de componentes del proyecto

--- src/App/index.js
+++ src/App/index.js
@@ -3,11 +3,53 @@ import { TodoProvider } from "./Context/todo";
 import { AppUI } from "./appUI";
 
 function App() {
+  const [state, setState] = React.useState("Estado compartido");
+
   return (
-    <TodoProvider>
-      <AppUI />
-    </TodoProvider>
+    <>
+      <TodoHeader>
+        <TodoCounter />
+        <TodoSearch />
+      </TodoHeader>
+
+      <TodoList>
+        <TodoItem state={state} />
+      </TodoList>
+    </>
   );
 }
+
+function TodoHeader({ children }) {
+  return <header>{children}</header>;
+}
+
+function TodoList({ children }) {
+  return <section>{children}</section>;
+}
+
+function TodoCounter() {
+  return <h1>0 de 2 completadas</h1>;
+}
+
+function TodoSearch() {
+  return <input placeholder="Cebolla" />;
+}
+
+function TodoItem({ state }) {
+  return (
+    <>
+      <input type="checkbox" />
+      <span>Todo Item - {state}</span>
+    </>
+  );
+}
+
+// function App() {
+//   return (
+//     <TodoProvider>
+//       <AppUI />
+//     </TodoProvider>
+//   );
+// }
 
 export default App;
