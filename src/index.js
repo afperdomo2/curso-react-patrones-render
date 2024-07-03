57fa4dd58f94a986f33370d8f28d20dd1c74d4ea
alvarado0925 <alvarado0925@gmail.com>
2024-07-02 23:59:45 -0500

    Haciendo ejemplos con Higher Order Component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import App from './App';
 //import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <App />
-);
+function App(props) {
+  return (
+    <h1>
+      ¡{props.saludo}, {props.nombre}!
+    </h1>
+  );
+}
+
+function withSaludo(WrappedComponent) {
+  return function WrapperComponentWithSaludo(saludo) {
+    return function ComponenteDeVerdad(props) {
+      return (
+        <>
+          <WrappedComponent {...props} saludo={saludo} />
+          <p>Estamos acompañando al WrappedComponent</p>
+        </>
+      );
+    };
+  };
+}
+
+const AppWithSaludo = withSaludo(App)("Hooooola");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppWithSaludo nombre="Pepito Pérez" />);
